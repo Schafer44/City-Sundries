1d859c8672379214d6df5e90bd0fee214ebf79a7
Schafer44 <tanner44schafer@gmail.com>
2021-07-21 06:36:34 -0500

    Working on parallax

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -37,31 +37,14 @@ export class Home extends React.Component {
                 {props => (
                      <animated.div style={props}>
                          
-                         <Parallax pages={2.45} style={{ top: '0', left: '0' }}>
+                         <Parallax pages={4.5} style={{ top: '0', left: '0' }}>
                          <ParallaxLayer offset={0} speed={.5} style={{ backgroundColor: '#ff6d6d' }} />
-                         <ParallaxLayer offset={0} speed={0} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                         <ParallaxLayer offset={.2} speed={0} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <div>
-                            <SimpleImageSlider
-                            style={{ maxWidth:'99%',maxHeight:'66%'}}
-                            width={this.state.width}
-                            height={this.state.height/1}
-                            images={IMANGES}
-                            showBullets={true}
-                            />
+                            <SimpleImageSlider style={{ maxWidth:'99%',maxHeight:'66%'}}width={this.state.width}height={this.state.height/1}images={IMANGES} showBullets={true}/>
                         </div>
                         </ParallaxLayer>
-                        
-                        
-  <ParallaxLayer
-    offset={1}
-    speed={1}
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      color: 'white',
-    }}>
-                            <div class='mainGrid'>
+                        <ParallaxLayer sticky={{ start: 1, end: 1.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-start',alignItems: 'center',color: 'white', }}>
                                 <div class='textboxLeft'>
                                     <div class='grid'>
                                     <p></p>
@@ -69,7 +52,11 @@ export class Home extends React.Component {
                                         <div class='ItemMain'>Stop by City Sundries in beautiful downtown Marquette for fresh-made sandwiches, unique gifts, and homemade ice cream! With original interior from its opening in the early 1900’s, the soda fountain and gift shop is a must see. Featuring original interior including soda fountain, marble tables, wood and glass cases, and tile floor. City Sundries is a unique and friendly space to spend the day. City Sundries can also take care of your special occasions with cards, same day balloon orders, and custom tee shirt designs! Local gift delivery is always free!</div>
                                     </div>
                                 </div>
+                            </ParallaxLayer>
+                            <ParallaxLayer sticky={{ start: 1, end: 1.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-end',alignItems: 'center',color: 'white', }}>
                                 <img src={Logo} class='imgRight' alt=""/>
+                            </ParallaxLayer>
+                            <ParallaxLayer sticky={{ start: 2, end: 2.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-end',alignItems: 'center',color: 'white', }}>
                                 <div class='textboxRight'>
                                     <div class='grid'>
                                         <p></p>
@@ -77,7 +64,12 @@ export class Home extends React.Component {
                                         <div class='ItemMain'> Tuesday 11-5 Wednesday 11-5 Thursday 11-5 Friday 11-5 Saturday 11-5 Closed Sunday and Monday</div>
                                     </div>
                                 </div>
+                            </ParallaxLayer>
+                            <ParallaxLayer sticky={{ start: 2, end: 2.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-start',alignItems: 'center',color: 'white', }}>
                                 <img src={Logo} class='imgLeft' alt=""/>
+                                </ParallaxLayer>
+                                <ParallaxLayer sticky={{ start: 3, end: 3.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-start',alignItems: 'center',color: 'white', }}>
+
                                 <div class='textboxLeftBottom'>
                                     <div class='grid'>
                                     <p></p>
@@ -85,8 +77,9 @@ export class Home extends React.Component {
                                         <div class='ItemMain'>104 N. Washington Street Marquette, Ks 67464</div>
                                     </div>
                                 </div>
+                            </ParallaxLayer>
+                            <ParallaxLayer sticky={{ start: 3, end: 3.5 }} speed={1}style={{ display: 'flex',justifyContent: 'flex-end',alignItems: 'center',color: 'white', }}>
                                 <img src={Logo} class='imgRightBottom' alt=""/>
-                            </div>
                             </ParallaxLayer>
                             </Parallax>
                         </animated.div>
