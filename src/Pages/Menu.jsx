ec7bca2ea2d069fbdabf673b4967a6ddce29ebf0
Schafer44 <tanner44schafer@gmail.com>
2021-07-28 08:38:06 -0500

    Added background to the menu

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Blackboard from './Pictures/fullMenu.jpeg';
+import Backboard from './Pictures/Backboard.jpg';
 export class Menu extends React.Component {
     render() {
         return (
             <>
                 <div class='blackboardDiv'>
-                    <img class='black' src={Blackboard} alt=""/>
+                    <img class='backBoard' src={Backboard} alt=""/>
+                    <img class='blackBoard' src={Blackboard} alt=""/>
                 </div>
             </>
         );
